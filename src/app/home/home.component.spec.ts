9c5429a878cd779d20486cc15fc1c274c267d461
RenatoValentim <rvrenato28@gmail.com>
2021-08-28 13:57:41 -0300

    test: ensure html estutures of the home.component

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,12 +9,14 @@ describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [HomeComponent],
+        imports: [],
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
@@ -24,5 +26,32 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render a HTML structure', () => {
+    const fixture = TestBed.createComponent(HomeComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const header = compiled.querySelector('header');
+    const section = compiled.querySelector('section');
+    const image = section?.querySelector('img');
+    const sectionDivs = section?.querySelectorAll('div');
+    const sectionFirstLineSpans = sectionDivs?.item(1).querySelectorAll('span');
+    const sectionSecondLineSpans = sectionDivs?.item(2).querySelectorAll('span');
+
+    expect(compiled.querySelector('main')).toBeTruthy();
+    expect(compiled.querySelector('main header')).toBeTruthy();
+    expect(compiled.querySelector('main section')).toBeTruthy();
+    expect(header).toBeTruthy();
+    expect(section).toBeTruthy();
+    expect(header?.querySelector('div h1')).toBeTruthy();
+    expect(header?.querySelector('div h2')).toBeTruthy();
+    expect(section?.querySelector('img')).toBeTruthy();
+    expect(sectionDivs?.length).toBe(3);
+    expect(sectionDivs?.item(1).querySelectorAll('span a').length).toBe(3);
+    expect(sectionDivs?.item(2).querySelectorAll('span a').length).toBe(3);
+    expect(sectionFirstLineSpans?.length).toBe(3);
+    expect(sectionSecondLineSpans?.length).toBe(3);
+    expect(image?.src.includes('world_white.png')).toBeTruthy();
   });
 });
