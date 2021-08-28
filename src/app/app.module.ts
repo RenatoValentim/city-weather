6807d6b4bc0a8b8e75ac3f5cf8334476ad47c95d
RenatoValentim <rvrenato28@gmail.com>
2021-08-28 12:48:47 -0300

    feat: add city.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HomeComponent
+      HomeComponent,
+      CityComponent
    ],
   imports: [
     BrowserModule,
