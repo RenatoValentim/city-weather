92e87730743cb07c7c09ec2c3b7adfa27a044be2
RenatoValentim <rvrenato28@gmail.com>
2021-08-29 01:40:04 -0300

    test: ensure that the city get a value on the ngOnInit

--- src/app/city/city.component.spec.ts
+++ src/app/city/city.component.spec.ts
@@ -1,14 +1,17 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRoute } from 'src/testing/activated-route-stub';
 import { ActivatedRouteStub } from '../../testing/activated-route-stub';
 import { CityComponent } from './city.component';
-import { ActivatedRoute } from 'src/testing/activated-route-stub';
-import { ParamMap } from '@angular/router';
+import { CityService } from '../shared/services/city.service';
+import { CITY_MOCK } from '../../testing/mocks/city.mock';
+import { of } from 'rxjs';
 
 describe('CityComponent', () => {
   let component: CityComponent;
   let fixture: ComponentFixture<CityComponent>;
   let activatedRoute: ActivatedRouteStub;
+  let cityServiceSpy: any;
 
   beforeEach(() => {
     activatedRoute = new ActivatedRouteStub();
@@ -16,10 +19,14 @@ describe('CityComponent', () => {
 
   beforeEach(
     waitForAsync(() => {
+      cityServiceSpy = jasmine.createSpyObj('CityService', ['loadBy']);
       activatedRoute.setParamMap({ cityName: 'Recife' });
       TestBed.configureTestingModule({
         declarations: [CityComponent],
-        providers: [{ provide: ActivatedRoute, useValue: activatedRoute }],
+        providers: [
+          { provide: ActivatedRoute, useValue: activatedRoute },
+          { provide: CityService, useValue: cityServiceSpy },
+        ],
       }).compileComponents();
     })
   );
@@ -43,7 +50,7 @@ describe('CityComponent', () => {
     expect(component.city).toBeFalsy();
   });
 
-  it('should have a city nama on the router params after ngOnInit', (done: DoneFn) => {
+  it('should have a city name on the router params after ngOnInit', (done: DoneFn) => {
     activatedRoute.paramMap.subscribe((value) => {
       expect(value).toBeTruthy();
 
@@ -52,6 +59,21 @@ describe('CityComponent', () => {
         expect(component.cityName).toBe('Recife');
       }
 
+      done();
+    });
+  });
+
+  it('should get a city after ngOnInit', (done: DoneFn) => {
+    activatedRoute.paramMap.subscribe((value) => {
+      expect(value).toBeTruthy();
+
+      if (value.has('cityName')) {
+        component.cityName = value.get('cityName');
+        expect(component.cityName).toBe('Recife');
+      }
+
+      component.city = cityServiceSpy.loadBy.and.returnValue(CITY_MOCK)();
+      expect(component.city).toBeTruthy();
       done();
     });
   });
